b7f260077f12575d69c1fb585057a6a2cdd2211f
yyana6 <adoros492@gmail.com>
2024-11-20 12:29:33 +0900

    Add files via upload

---
+++ 1задание/1задание/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class progect
+{
+    public int x;
+    private int y;
+    public int Add (int e, int i)
+    {
+        x = e; y = i;
+        return x+y;
+    }
+    public void res()
+    {
+        Console.WriteLine($"Результат операции:{y}");
+    }
+    public static void Main (string[] args)
+    {
+        progect progect = new progect();
+        int sum = progect.Add(7, 4);
+        Console.WriteLine($"Результат:{ sum}");
+        progect.res();
+    } 
+}
